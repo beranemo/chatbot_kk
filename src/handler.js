21bf73a22c756f9afb75145660ae1e6da1438b83
beranemo <beranemo@gmail.com>
2018-11-10 12:09:25 +0800

    Impletement for olami handler

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,11 @@
 const {LineHandler} = require('bottender')
+const olami = require('./nlp/Olami')
 
 exports.lineHandler = new LineHandler()
   .onText(async context => {
       const text = context.event.text
-      const reply = text
+      const userId = context._session.user.id
+      const reply = await olami.nli(text, userId)
       await context.replyText(reply)
     }
   )
